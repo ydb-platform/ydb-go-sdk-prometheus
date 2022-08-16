3c38944aea174beb0b9df7ce668e3b9b0e71bc2a
asmyasnikov <79263256394@ya.ru>
2022-08-16 22:06:48 +0300

    fix

--- sql.go
+++ sql.go
@@ -1,12 +1,13 @@
 package metrics
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
 	metrics "github.com/ydb-platform/ydb-go-sdk-metrics"
 	"github.com/ydb-platform/ydb-go-sdk/v3/trace"
 )
 
 // DatabaseSQL makes trace.DatabaseSQL with core metrics publishing
-func DatabaseSQL(registry coreMetrics.Registry, opts ...option) trace.DatabaseSQL {
+func DatabaseSQL(registry prometheus.Registerer, opts ...option) trace.DatabaseSQL {
 	c := &config{
 		registry:  registry,
 		namespace: defaultNamespace,
